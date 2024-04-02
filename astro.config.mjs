a07e4eec9e6626122407aa893126c006bc6e12af
iborntowin <nassim28maaoui@gmail.com>
2024-04-02 12:40:02 +0100

    pushing

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://iborntowin.github.io",
+  base: "https://github.com/iborntowin/Portfolio.github.io",
+});
